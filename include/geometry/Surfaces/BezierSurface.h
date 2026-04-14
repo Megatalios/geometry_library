21c51bf60921c6f30e8bd820383d813c4baa4412
Kamil <kamil.nabyev2003@gmail.com>
2026-04-14 12:06:54 +0500

    Добавлено начало поверхности Безье

--- include/geometry/Surfaces/BezierSurface.h
+++ include/geometry/Surfaces/BezierSurface.h
@@ -1,5 +1,22 @@
-#include "ISurface.h"
+#pragma once
+#include "geometry/Surfaces/ISurface.h"
+#include "geometry/Curves/BezierCurve.h"
+#include <vector>
 
 class BezierSurface : public ISurface {
+	// Матрица точек (сетка)
+	std::vector<std::vector<Point3D>> control_net;
+public:
+	// Конструкторы
+	BezierSurface();
+	BezierSurface(const std::vector<std::vector<Point3D>>& net);
+	BezierSurface(const BezierSurface& otherSurface);
+	// Получить точку на поверхности по параметрам u, v
+	Point3D get_point(double u, double v) const override;
 
+	// Получить вектор нормали к точке с параметрами u, v
+	Vector3D get_norm(double u, double v) const override;
+
+	// Виртуальный деструктор - нужен для корректного удаления объекта, который данный интерфейс поддерживает
+	~BezierSurface() = default;
 };
--- src/Surfaces/BezierSurface.cpp
+++ src/Surfaces/BezierSurface.cpp
@@ -1,1 +1,36 @@
+// BezierSurface.cpp
 #include "geometry/Surfaces/BezierSurface.h"
+
+BezierSurface::BezierSurface() {}
+BezierSurface::BezierSurface(const std::vector<std::vector<Point3D>>& net) :control_net(net) {}
+BezierSurface::BezierSurface(const BezierSurface& otherSurface) : control_net(otherSurface.control_net) {}
+
+// Получить точку на поверхности по параметрам u, v
+Point3D BezierSurface::get_point(double u, double v) const {
+	Point3D result(0, 0, 0);
+	if (control_net.empty() || control_net[0].empty()) {
+		return result;
+	}
+	// Сначала создаем одну кривую из точек, которая получается путем нахождения точек u в каждой созданной кривой
+	int rows = control_net.size();
+	std::vector<Point3D> temp_u_points;
+	for (int i = 0; i < rows; ++i) {
+		BezierCurve temp_curve(control_net[i]);
+		temp_u_points.push_back(temp_curve.get_point(u));
+	}
+	// Затем в этой кривой находим уже точку с параметром v
+	BezierCurve temp_v_points(temp_u_points);
+	result = temp_v_points.get_point(v);
+
+	return result;
+}
+
+// Получить вектор нормали к точке с параметрами u, v
+Vector3D BezierSurface::get_norm(double u, double v) const {
+	Vector3D result(0, 0, 1);
+	if (control_net.empty() || control_net[0].empty()) {
+		return result;
+	}
+	// Что-то еще 
+	return result;
+}
