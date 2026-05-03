d45ef3a28570554fe0cc109eda30f15428e4ad12
Kamil <kamil.nabyev2003@gmail.com>
2026-05-03 17:24:05 +0500

    Положено начало работы с AABB

--- src/BoundingBox/BoundingBox.cpp
+++ src/BoundingBox/BoundingBox.cpp
@@ -1,1 +1,29 @@
 #include "geometry/BoundingBox/BoundingBox.h"
+
+// Конструктор по умолчанию 
+BoundingBox::BoundingBox(): minPoint(), maxPoint(), is_empty(false) {}
+
+// Конструктор с параметрами
+BoundingBox::BoundingBox(const Point3D& minP, const Point3D& maxP) : minPoint(minP), maxPoint(maxP), is_empty(false) {}
+
+// Конструктор копирования
+BoundingBox::BoundingBox(const BoundingBox& otherBox): minPoint(otherBox.minPoint), maxPoint(otherBox.maxPoint), is_empty(otherBox.is_empty) {}
+
+
+// Метод для расширения коробки новой точкой
+void BoundingBox::add_point(const Point3D& point) {
+
+}
+
+// Геттеры
+Point3D BoundingBox::get_min_point() const {
+	return minPoint;
+}
+Point3D BoundingBox::get_max_point() const {
+	return maxPoint;
+}
+
+// Проверка пересечения с другой коробкой
+bool BoundingBox::is_intersect(const BoundingBox& other) const {
+
+}
