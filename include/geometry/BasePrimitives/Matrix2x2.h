9d763678c4e08c77e989db55ee805092d419ae9c
Kamil <kamil.nabyev2003@gmail.com>
2026-05-28 21:08:31 +0500

    Добавлена логика для решения матричных уравнений 2 на 2 методом Крамера

---
+++ include/geometry/BasePrimitives/Matrix2x2.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Matrix2x2
+{
+	// Сама матрица
+	double m[2][2];
+
+public:
+	// Конструкторы
+	// По умолчанию
+	Matrix2x2();
+	// С параметрами (элементы передаются в виде 4 отдельных аргументов)
+	Matrix2x2(double m00, double m01, double m10, double m11);
+	// Конструктор копирования
+	Matrix2x2(const Matrix2x2& other);
+
+	// Операция сложения
+	Matrix2x2 operator+(const Matrix2x2& other) const;
+
+	// Вычисление определителя
+	double det() const; 
+
+	// Решение СЛАУ методом Крамера (m * x = b)
+	// Возвращает true, если решение найдено (определитель не близок к нулю)
+	// Результаты записываются по ссылкам в out_x и out_y
+	bool solveCramer(double b0, double b1, double& out_x, double& out_y) const;
+};
---
+++ src/BasePrimitives/Matrix2x2.cpp
@@ -0,0 +1,27 @@
+#include "geometry/BasePrimitives/Matrix2x2.h"
+
+// Конструкторы
+Matrix2x2::Matrix2x2() : m{ {1, 0}, {0, 1} } {}
+Matrix2x2::Matrix2x2(double m00, double m01, double m10, double m11) : m{ {m00, m01}, {m10, m11} } {}
+Matrix2x2::Matrix2x2(const Matrix2x2& other) : m{ {other.m[0][0], other.m[0][1]}, {other.m[1][0], other.m[1][1]} } {}
+
+Matrix2x2 Matrix2x2::operator+(const Matrix2x2& other) const {
+	return Matrix2x2(
+		m[0][0] + other.m[0][0], m[0][1] + other.m[0][1],
+		m[1][0] + other.m[1][0], m[1][1] + other.m[1][1]
+	);
+}
+
+double Matrix2x2::det() const {
+	return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+}
+
+bool Matrix2x2::solveCramer(double b0, double b1, double& out_x, double& out_y) const {
+	double determinant = Matrix2x2::det();
+	if (determinant == 0) {
+		return false;
+	}
+	out_x = (b0 * m[1][1] - b1 * m[0][1]) / determinant;
+	out_y = (m[0][0] * b1 - m[1][0] * b0) / determinant;
+	return true;
+}
